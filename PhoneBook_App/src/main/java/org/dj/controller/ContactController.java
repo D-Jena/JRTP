81d0306ddac7613d239eed9100688779009669e1
Acer <Acer@Acer>
2022-12-20 21:44:24 +0530

    complied but not runed.

--- PhoneBook_App/src/main/java/org/dj/controller/ContactController.java
+++ PhoneBook_App/src/main/java/org/dj/controller/ContactController.java
@@ -5,6 +5,9 @@ import org.dj.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ContactController {
@@ -12,10 +15,23 @@ public class ContactController {
 	@Autowired
 	private ContactService contactService;
 	
+	@GetMapping(value = {"/"})
 	public String loadForm(Model model) {
 		Contact c = new Contact();
 		model.addAttribute("Contact",c);
-		return "addContact";
+		return "contactInfo";
+	}
+	
+	@PostMapping(value = "/saveContact")
+	public String handleSbmtBtn(@ModelAttribute("contact") Contact c, Model model) {
+		boolean isSaved = contactService.saveContact(c);
+		if(isSaved)
+			model.addAttribute("succMsg", "Contact saved");
+		else 
+			model.addAttribute("errMsg", "Faild to save contact");
+		
+		return "contactInfo";
+		
 	}
 
-}
+}//class
---
+++ PhoneBook_App/src/main/webapp/views/contactInfo.jsp
@@ -0,0 +1,45 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<h2>Save contact</h2>
+	<p>
+		<font color='green'>${succMsg}</font>
+	</p>
+	<p>
+		<font color='red'>${errMsg}</font>
+	</p>
+	<form:form  action="saveContact" 
+				modelAttribute="contact" 
+				method="POST">
+		<table>
+			<tr>
+				<td>Contact Name</td>
+				<td><form:input path="name"/></td>
+			</tr>
+			<tr>
+				<td>Contact Email</td>
+				<td><form:input path="email"/></td>
+			</tr>
+			<tr>
+				<td>Contact Number</td>
+				<td><form:input path="phoneNo"/></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="Save"/></td>
+				<td><input type="reset" value="Reset"/></td>
+			</tr>
+		</table>
+	</form:form>
+
+</body>
+</html>
